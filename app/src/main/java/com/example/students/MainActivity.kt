42de848f40a69f3a3b04fa0a19bdac2e319b68ec
ElioHoyos <elio18david@gmail.com>
2026-09-08 20:57:12 -0500

    fix: error al mostrar datos de la bd

--- app/src/main/java/com/example/students/MainActivity.kt
+++ app/src/main/java/com/example/students/MainActivity.kt
@@ -1,20 +1,39 @@
 package com.example.students
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.students.db.Config
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        //invocamos al metodo
+        cargarAlumnosDesdeDb()
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
+
+    private fun cargarAlumnosDesdeDb(){
+        Thread{
+            try {
+                val config = Config(this)
+                val lista = config.obtenerAlumnos()
+                Log.d("PRUEBA_SQLite","Carga Completa recibiba MainActivity. Total: ${lista.size}")
+            }catch (e: Exception){
+                Log.e("PRUEBA_SQLite","Error desde MainActivity: ${e.message}")
+            }
+        }.start()
+    }
+
 }
