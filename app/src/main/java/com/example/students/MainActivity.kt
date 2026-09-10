fd318cc8c66cc8c55c752855781e0692365aba91
ElioHoyos <elio18david@gmail.com>
2026-09-09 21:45:48 -0500

    fix: se muestra data de la bd

--- app/src/main/java/com/example/students/MainActivity.kt
+++ app/src/main/java/com/example/students/MainActivity.kt
@@ -6,13 +6,30 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.students.db.Config
+import com.example.students.iu.AlumnoAdapter
 
 class MainActivity : AppCompatActivity() {
+
+    //1. Declarar variables para el RecyclerView y el Adapter
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: AlumnoAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+
+        //2. Vinculamos el RecyclerView del XML y asignamos un layoutManager
+        recyclerView = findViewById(R.id.recyclerViewAlumnos)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        //3.Creamos el adapter inicialmente vacio
+        adapter = AlumnoAdapter(emptyList())
+        recyclerView.adapter = adapter
 
         //invocamos al metodo
         cargarAlumnosDesdeDb()
@@ -30,6 +47,11 @@ class MainActivity : AppCompatActivity() {
                 val config = Config(this)
                 val lista = config.obtenerAlumnos()
                 Log.d("PRUEBA_SQLite","Carga Completa recibiba MainActivity. Total: ${lista.size}")
+
+                runOnUiThread {
+                    adapter.updateData(lista)
+                }
+
             }catch (e: Exception){
                 Log.e("PRUEBA_SQLite","Error desde MainActivity: ${e.message}")
             }
